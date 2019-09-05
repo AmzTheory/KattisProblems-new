a20ef46e4318e0a979d69f93c1c86088e39f04e6
ArsUAE <ahmedamz@outlook.com>
2019-09-05 19:06:24 +0100

    first two problems

---
+++ carrots.py
@@ -0,0 +1,12 @@
+# https://open.kattis.com/problems/carrot
+def main():
+    line=input().split(" ")
+    nContestants=int(line[0])
+    nCarrots=line[1]
+
+    for i in range(nContestants):
+        input()
+    print(nCarrots)
+
+##run the main function
+main()
---
+++ timeloop.py
@@ -0,0 +1,9 @@
+#https://open.kattis.com/problems/timeloop
+
+def main():
+    n=int(input())
+    for i in range(n):
+        print (str(i+1), "Abracadabra")
+
+#call main
+main()
